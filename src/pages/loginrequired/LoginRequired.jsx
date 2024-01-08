ab17bb6abcbbba458648952875dfef26e42ceda9
mandylenssen <mandylenssen@gmail.com>
2024-01-08 16:07:44 +0100

    Login required pagina afgerond

--- src/pages/loginrequired/LoginRequired.jsx
+++ src/pages/loginrequired/LoginRequired.jsx
@@ -1,10 +1,14 @@
 import './LoginRequired.css'
+import {Link} from "react-router-dom";
 
 function LoginRequired() {
     return (
-        <>
-            <h1>Login Required</h1>
-        </>
+      <section className="login-required-section outer-container">
+          <div className="green-text inner-container">
+              <h8>In order to view this page you need to be logged in.</h8>
+              <h8>Click <Link to="/login">here</Link> to log in or create an account.</h8>
+          </div>
+      </section>
     )
 }
 
