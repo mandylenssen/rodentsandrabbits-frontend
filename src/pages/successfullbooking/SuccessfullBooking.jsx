9c04587aec112f9b2589abd5747ddda9b4567d2c
mandylenssen <mandylenssen@gmail.com>
2024-01-08 16:31:33 +0100

    account created pagina afgerond

--- src/pages/successfullbooking/SuccessfullBooking.jsx
+++ src/pages/successfullbooking/SuccessfullBooking.jsx
@@ -1,11 +1,19 @@
 import './SuccessfullBooking.css'
+import {NavLink} from "react-router-dom";
+import Button from "../../components/button/Button.jsx";
 
 function SuccessfullBooking() {
     return (
-        <>
-            <h8>Your reservation has been successfully confirmed!</h8>
-            <button>my reservations</button>
-        </>
+
+        <section className="successfull-booking-section outer-container">
+            <div className="yellow-text inner-container">
+                <h8>Your booking has been successfully confirmed!</h8>
+                <NavLink to="/mybookings">
+                    <Button type="button" color="primary">My bookings</Button>
+                </NavLink>
+
+            </div>
+        </section>
     )
 }
 
