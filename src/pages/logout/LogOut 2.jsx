4562299480b4029d0ff3f4a1819e4c3c98a294c9
mandylenssen <mandylenssen@gmail.com>
2024-01-26 11:05:21 +0100

    Begonnen aan de login functie werkende te krijgen richting de backend

---
+++ src/pages/logout/LogOut 2.jsx
@@ -0,0 +1,12 @@
+import './LogOut.css';
+function LogOut() {
+
+return (
+<>
+    <div className="outer-container">
+    <h3 className="test">you successfully logged out</h3>
+    </div>
+</>
+);
+}
+export default LogOut;
