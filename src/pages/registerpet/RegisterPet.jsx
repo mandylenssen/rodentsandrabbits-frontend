fdae2c13b87b18c806d3daeaeb844d94176526c6
mandylenssen <mandylenssen@gmail.com>
2024-03-07 14:19:51 +0100

    semantische onderdelen gewijzigd en correct benaming gegeven.

--- src/pages/registerpet/RegisterPet.jsx
+++ src/pages/registerpet/RegisterPet.jsx
@@ -1,11 +1,12 @@
 import "./RegisterPet.css"
 import Button from "../../components/button/Button.jsx";
 import {useForm} from "react-hook-form";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {addMonths, addYears} from "date-fns";
-import {NavLink, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import axios from "axios";
 import {jwtDecode} from "jwt-decode";
+import {validateDateOfBirth, validatePhoto} from "../../utilities/formValidation.jsx";
 
 function RegisterPet() {
     const {
@@ -13,58 +14,57 @@ function RegisterPet() {
         formState: {errors},
         register,
         watch,
-    } = useForm({mode: "onBlur"});
+    } = useForm({mode: 'onBlur'});
 
     const navigate = useNavigate();
     const source = axios.CancelToken.source();
-    const [errorText, setErrorText] = useState("");
-    const jwtToken = localStorage.getItem("token");
+    const [errorText, setErrorText] = useState('');
+    const jwtToken = localStorage.getItem('token');
 
-    const validatePhoto = (value) => {
-        if (value && value.length > 0) {
-            const file = value[0];
-            const fileName = file.name;
-            if (fileName) {
-                const allowedExtensions = ["jpg", "jpeg", "png"];
-                const fileExtension = fileName.split(".").pop().toLowerCase();
-                if (!allowedExtensions.includes(fileExtension)) {
-                    return "Invalid file format. Please upload a JPG or PNG file.";
-                }
-                const maxFileSize = 5 * 1024 * 1024;
-                if (file.size > maxFileSize) {
-                    return "File is too large. Max 5MB allowed.";
-                }
-            }
-            return true;
-        }
-    }
-
-    const validateDateOfBirth = (value) => {
-        const dateOfBirth = new Date(value);
-        const minDate = addYears(new Date(), -20);
-        const maxDate = addMonths(new Date(), -3);
-
-        if (dateOfBirth < minDate) {
-            return "Date of birth must be after 01-01-2000";
-        }
-
-        if (dateOfBirth > maxDate) {
-            return "Date of birth must be before 31-12-2022";
-        }
-
-        return true;
-    };
+    // const validatePhoto = (value) => {
+    //     if (value && value.length > 0) {
+    //         const file = value[0];
+    //         const fileName = file.name;
+    //         if (fileName) {
+    //             const allowedExtensions = ['jpg', 'jpeg', 'png'];
+    //             const fileExtension = fileName.split('.').pop().toLowerCase();
+    //             if (!allowedExtensions.includes(fileExtension)) {
+    //                 return 'Invalid file format. Please upload a JPG or PNG file.';
+    //             }
+    //             const maxFileSize = 5 * 1024 * 1024;
+    //             if (file.size > maxFileSize) {
+    //                 return 'File is too large. Max 5MB allowed.';
+    //             }
+    //         }
+    //         return true;
+    //     }
+    // }
+    //
+    // const validateDateOfBirth = (value) => {
+    //     const dateOfBirth = new Date(value);
+    //     const today = new Date();
+    //     const minDate = addYears(today, -15);
+    //     const maxDate = addMonths(today, -3);
+    //     if (dateOfBirth < minDate) {
+    //         return 'Date of birth must be less than 15 years ago.';
+    //     }
+    //     if (dateOfBirth > maxDate) {
+    //         return 'Date of birth must be at least 3 months ago.';
+    //     }
+    //
+    //     return true;
+    // };
 
 
     async function handleFormSubmit(data) {
         try {
-            console.log("Form data:", data);
+            console.log('Form data:', data);
             const decodedToken = jwtDecode(jwtToken);
             const ownerUsername = decodedToken.sub;
 
-            const result = await axios.post("http://localhost:8080/pets", {
+            const result = await axios.post('http://localhost:8080/pets', {
                 name: data.name,
-                birthday: data["date-of-birth"],
+                birthday: data['date-of-birth'],
                 species: data.species,
                 gender: data.gender,
                 details: data.details,
@@ -74,26 +74,24 @@ function RegisterPet() {
                 ownerUsername: ownerUsername
             }, {
                 headers: {
-                    "Authorization": `Bearer ${jwtToken}`,
-                    "Content-Type": "application/json"
+                    'Authorization': `Bearer ${jwtToken}`,
+                    'Content-Type': 'application/json'
                 },
                 cancelToken: source.token,
             });
-            console.log("Pet added successfully:", result.data);
+            console.log('Pet added successfully:', result.data);
             const petId = result.data.id;
             const photoFile = data.photo;
 
             if (photoFile && photoFile.length > 0) {
                 await uploadProfileImage(petId, photoFile);
-                navigate("/mypets");
             } else {
-                console.log("No photo to upload");
-                navigate("/mypets");
+                console.log('No photo to upload');
             }
-            navigate("/mypets");
+            navigate('/mypets');
         } catch (error) {
-            console.error("Error adding pet:", error.response?.data);
-            console.log("Error message:", error.message);
+            console.error('Error adding pet:', error.response?.data);
+            console.log('Error message:', error.message);
             setErrorText(error.response?.data?.message);
 
         }
@@ -102,137 +100,155 @@ function RegisterPet() {
 
     const uploadProfileImage = async (petId, photoFile) => {
         const formData = new FormData();
-        formData.append("file", photoFile[0]);
+        formData.append('file', photoFile[0]);
 
         try {
             await axios.post(`http://localhost:8080/pets/${petId}/profileImage`, formData, {
                 headers: {
-                    "Authorization": `Bearer ${jwtToken}`,
-                    "Content-Type": "multipart/form-data"
+                    'Authorization': `Bearer ${jwtToken}`,
+                    'Content-Type': 'multipart/form-data'
                 },
             });
-            console.log("Profile image uploaded successfully");
+            console.log('Profile image uploaded successfully');
         } catch (error) {
-            console.error("Error uploading profile image:", error);
+            console.error('Error uploading profile image:', error);
         }
     };
 
     return (
         <>
 
-            <form className="register-pet-container outer-container" onSubmit={handleSubmit(handleFormSubmit)}>
-                <div className="register-pet-inner-container">
+            <main className="register-pet-outer-container outer-container">
+                <form className="register-pet-inner-container" onSubmit={handleSubmit(handleFormSubmit)}>
                     <h3>Register pet</h3>
 
-                    <div className="top-form-entry-wrapper">
-                        <div className="form-group">
+                    <section className="top-form-entry-wrapper">
+                        <div className="top-form-fields">
                             <label htmlFor="name-field">
-                                <span>name*</span>
+                                <span className="register-pet-value-field">name*</span>
                                 <input type="text" id="name-field"
                                        {...register("name", {
                                            required: {value: true, message: "name is required",},
                                            minLength: {value: 2, message: "name must contain at least 2 characters",},
-                                           maxLength: {value: 20, message: "name can contain a maximum of 20 characters",
+                                           maxLength: {
+                                               value: 20, message: "name can contain a maximum of 20 characters",
                                            },
                                        })}
                                        placeholder="enter pet name"/>
                                 {errors.name && <p className="error-text">{errors.name.message}</p>}
                             </label></div>
 
-                        <div className="form-group">
-                            <label htmlFor="date-of-birth-field">
-                                <span>date of birth*</span>
-                                <input type="date" id="date-of-birth-field"
-                                       {...register("date-of-birth", {
-                                           required: {value: true, message: "Date of birth is required",},
-                                           validate: validateDateOfBirth,
-                                       })}
-                                />
-                                {errors["date-of-birth"] &&
-                                    <p className="error-text">{errors["date-of-birth"].message}</p>}
-                            </label>
-                        </div>
+                        <label htmlFor="date-of-birth-field">
+                            <span className="register-pet-value-field">date of birth*</span>
+                            {/*    <input type="date" id="date-of-birth-field"*/}
+                            {/*           {...register("date-of-birth", {*/}
+                            {/*               required: {value: true, message: "Date of birth is required",},*/}
+                            {/*               validate: validateDateOfBirth,*/}
+                            {/*           })}*/}
+                            {/*    />*/}
+                            {/*    {errors["date-of-birth"] &&*/}
+                            {/*        <p className="error-text">{errors["date-of-birth"].message}</p>}*/}
 
-                        <div className="form-group">
-                            <label htmlFor="species-field">
-                                <span>species*</span>
-                                <select id="species-field" {...register("species", {required: true})}
-                                        defaultValue={watch("species") || ""}>
-                                    <option value="" disabled>select species</option>
-                                    <option value="rabbit">Rabbit</option>
-                                    <option value="hamster">Hamster</option>
-                                    <option value="rat">Rat</option>
-                                    <option value="mouse">Mouse</option>
-                                    <option value="gerbil">Gerbil</option>
-                                    <option value="guinea-pig">Guinea pig</option>
-                                    <option value="chinchilla">Chinchilla</option>
-                                </select>
-                                {errors["species"] && <p className="error-text">species is required</p>}
-                            </label></div>
+                            <input type="date" id="date-of-birth-field"
+                                   {...register("date-of-birth", {
+                                       required: {value: true, message: "Date of birth is required"},
+                                       validate: validateDateOfBirth,
+                                   })}
+                            />
+                            {errors["date-of-birth"] && <p className="error-text">{errors["date-of-birth"].message}</p>}
 
-                        <div className="form-group">
-                            <label htmlFor="gender-field">
-                                <span>gender*</span>
-                                <select id="gender-field" {...register("gender", {required: true})}
-                                        defaultValue={watch("gender") || ""}>
-                                    <option value="" disabled>Select gender</option>
-                                    <option value="female">Female</option>
-                                    <option value="male">Male</option>
-                                </select>
-                                {errors["gender"] && <p className="error-text">gender is required</p>}
-                            </label>
-                        </div>
-                    </div>
-                    <div className="field-wrapper">
+                        </label>
+
+
+                        <label htmlFor="species-field">
+                            <span className="register-pet-value-field">species*</span>
+                            <select id="species-field" {...register("species", {required: true})}
+                                    defaultValue={watch("species") || ""}>
+                                <option value="" disabled>select species</option>
+                                <option value="rabbit">Rabbit</option>
+                                <option value="hamster">Hamster</option>
+                                <option value="rat">Rat</option>
+                                <option value="mouse">Mouse</option>
+                                <option value="gerbil">Gerbil</option>
+                                <option value="guinea-pig">Guinea pig</option>
+                                <option value="chinchilla">Chinchilla</option>
+                            </select>
+                            {errors["species"] && <p className="error-text">species is required</p>}
+                        </label>
+                        <label htmlFor="gender-field">
+                            <span className="register-pet-value-field">gender*</span>
+                            <select id="gender-field" {...register("gender", {required: true})}
+                                    defaultValue={watch("gender") || ""}>
+                                <option value="" disabled>Select gender</option>
+                                <option value="female">Female</option>
+                                <option value="male">Male</option>
+                            </select>
+                            {errors["gender"] && <p className="error-text">gender is required</p>}
+                        </label>
+                    </section>
+                    <div className="bottom-field-wrapper">
                         <label htmlFor="medication-field">
-                            <span>medication</span>
+                            <span className="register-pet-value-field">medication</span>
                             <textarea id="medication-field" rows="4" cols="40"
                                       {...register("medication")}>
                         </textarea>
                         </label>
 
                         <label htmlFor="special-notes-field">
-                            <span>special notes</span>
+                            <span className="register-pet-value-field">special notes</span>
                             <textarea id="special-notes-field" rows="4" cols="40"
                                       {...register("details")}>
                         </textarea>
                         </label>
 
                         <label htmlFor="diet-field">
-                            <span>diet</span>
+                            <span className="register-pet-value-field">diet</span>
                             <textarea id="diet-field" rows="4" cols="40"
                                       {...register("diet")}>
                         </textarea>
                         </label>
                         <span>upload picture</span>
-                        <div className="custom-file-upload">
-
-                            <div className="upload-header">
+                        <div className="pet-photo-upload-bar">
+                            <div className="pet-photo-upload-content">
 
                                 <label htmlFor="photo-field" className="upload-button">
-                                    <span>choose file</span>
+                                    <span className="register-pet-value-field">choose file</span>
+                                    {/*            <input type="file" id="photo-field"*/}
+                                    {/*                   {...register("photo", {*/}
+                                    {/*                       required: {*/}
+                                    {/*                           value: true,*/}
+                                    {/*                           message: "Please upload a photo of your pet",*/}
+                                    {/*                       },*/}
+                                    {/*                       validate: validatePhoto,*/}
+                                    {/*                   })}*/}
+                                    {/*            />*/}
+                                    {/*        </label>*/}
+                                    {/*        <span*/}
+                                    {/*            className="upload-file-name">{watch("photo")?.[0]?.name || "No file chosen"}</span>*/}
+                                    {/*        {errors.photo && <p className="error-text">{errors.photo.message}</p>}*/}
+                                    {/*    </div>*/}
+
                                     <input type="file" id="photo-field"
                                            {...register("photo", {
                                                required: {
                                                    value: true,
                                                    message: "Please upload a photo of your pet",
                                                },
-                                               validate: validatePhoto,
+                                               validate: validatePhoto, // Ensure this function is being invoked correctly
                                            })}
-                                    />
-                                </label>
-                                <span className="file-name">{watch("photo")?.[0]?.name || "No file chosen"}</span>
-                                {errors.photo && <p className="error-text">{errors.photo.message}</p>}
+
+                                    /> </label>
+                                    {errors.photo && <p className="error-text">{errors.photo.message}</p>}
+
                             </div>
-
-
-                        </div>
-                        <Button color="secondary" type="submit">save</Button>
-                    </div>
-                </div>
-            </form>
+                            </div>
+                            <div className="button-wrapper">
+                                <Button color="secondary" type="submit">save</Button></div>
+                            </div>
+                </form>
+            </main>
         </>
-    )
+)
 }
 
 export default RegisterPet;
